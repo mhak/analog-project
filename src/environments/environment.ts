3f1f06aafdeae42ed8c99d27b995cfc6436d195a
Meng Hak <meng.hak@xcentium.com>
2024-07-08 14:19:19 -0700

    .

---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+
+export const environment = {
+  production: true
+};
+
+export const Config = {
+  api_key: '123',
+  delivery_token: '123',
+  environment: 'CONTENTSTACK_ENVIRONMENT',
+  branch: 'main',
+  region: 'us',
+  preview_token:'undefined',
+  preview_host:'undefined',
+  api_host:'undefined',
+  app_host:'undefined',
+  live_preview:true,
+  test: 'test',
+  meng: 'meng',
+};
