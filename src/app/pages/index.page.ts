bb7d7857ec4b5b80a48b7080eddff836c07710ab
Meng Hak <meng.hak@xcentium.com>
2024-07-09 16:14:33 -0700

    fix build error

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -1,6 +1,5 @@
 import { Component, signal, inject } from '@angular/core';
 import { config } from '../../environments/environment';
-import { ContentstackQueryService } from '../../modules/contentstack/cs.query.service';
 //import { AsyncPipe } from '@angular/common';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { injectLoad } from '@analogjs/router';
@@ -27,28 +26,16 @@ import { load } from './index.server'; // not included in client build
 })
 
 export default class HomeComponent {
-  //private cs = inject(ContentstackQueryService);
   serverData = toSignal(injectLoad<typeof load>(), { requireSync: true });
-  private csContent = {};
 
   count = signal(0);
   display = config.test;
   constructor() {
-    //this.getEntry();
+
   }
   
   increment() {
     this.count.update((count) => count + 1);
   }
 
-  getEntry() {
-    this.cs.getEntryWithQuery('page', { key: 'url', value: '/' }).then((response) => {
-      this.csContent = response[0][0];
-      console.log('this.csContent', this.csContent);
-
-    })
-  }
-
-  ngOnInit() {
-  }
 }
