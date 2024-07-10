bb63cae7458e57e27cfc6255c17aaf10bf94649b
Ashok Gelal <ashokgelal@gmail.com>
2024-07-10 14:33:04 -0400

    doc: update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,6 +13,6 @@ export const config = {
   preview_host:'',
   api_host:'api.contentstack.io',
   app_host:'app.contentstack.com',
-  live_preview:true,
+  live_preview:false,
   test: 'local',
 };
